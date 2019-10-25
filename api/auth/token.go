03e4d33dc03607e28fb11d2fef9c39eed5d50b43
rabiul islam <rabiulislam993@live.com>
2019-10-25 18:42:45 +0600

    fixed return error

--- api/auth/token.go
+++ api/auth/token.go
@@ -19,7 +19,7 @@ func CreateToken(user_id uint32) (string, error) {
 	claims["user_id"] = user_id
 	claims["exp"] = time.Now().Add(time.Hour * 1).Unix() //Token expires after 1 hour
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	return token.SignedString([]byte(os.Getenv("API_SECRET")))
+	return token.SignedString([]byte(os.Getenv("API_SECRET"))), nil
 
 }
 
